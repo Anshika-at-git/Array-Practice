b4f4d2513c17706f78ed8830f786902f5bed0740
Anshika Singh <anshikasingh082005@gmail.com>
2025-08-23 13:57:21 +0530

    ArrayInReverse.cpp

--- ArrayInReverse.cpp
+++ ArrayInReverse.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int main(){
     int arr[5] = {10, 20, 30, 40, 50};
-    for(int i=4; i>=0; i--){
+    int n=5;
+    for(int i=n-1; i>=0; i--){
         cout<<arr[i]<<" ";
     }
     return 0;
+
 }
