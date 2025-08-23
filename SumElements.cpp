7ae0516fb903b50dace167ed48a7f016b77b1902
Anshika Singh <anshikasingh082005@gmail.com>
2025-08-23 13:49:45 +0530

    SumElements.cpp

--- SumElements.cpp
+++ SumElements.cpp
@@ -5,7 +5,7 @@ int main(){
     int num[4]= {11, 22, 33, 44};
     int sum = 0;
     for(int i=0; i<4; i++){
-        sum+=num[i];
+        sum+=num[i];         
     }
     cout<<"The sum of elements is "<<sum;
     return 0;
