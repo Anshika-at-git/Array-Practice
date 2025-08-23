c1757a415f69efc2bef0942d7341597711878b6d
Anshika Singh <anshikasingh082005@gmail.com>
2025-08-23 13:56:07 +0530

    Add files via upload

---
+++ ArrayInReverse.cpp
@@ -0,0 +1,12 @@
+// Printing a given array in reverse
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5] = {10, 20, 30, 40, 50};
+    for(int i=4; i>=0; i--){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
